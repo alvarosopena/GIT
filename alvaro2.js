280c7cee8a3ae58890518b0b075432d761ededa3
--global <alvarosopenaf@gmail.com>
2020-11-16 21:31:30 -0300

    otra modificacion alvaro2 por alvaro

--- alvaro2.js
+++ alvaro2.js
@@ -8,3 +8,31 @@ const numeros = [1,2,3]
 console.log (numeros)
 // hasta aca
 >>>>>>> 145c1d15546a7144ef5c1b75d2ce51483260043d
+
+//parte 2 agregada Alvaro
+let autos = [{
+    marca: "Ford",
+    modelo:"Fiesta",
+    precio:150000,
+    km:200,
+    color:"Azul",
+    cuotas:12,
+    anio:2019,
+    patente:"APL123",
+    vendido:false },
+    {
+       marca: "Toyota",
+    modelo:"Corolla",
+    precio:100000,
+    km:0,
+    color:"Blanco",
+    cuotas:14,
+    anio:2019,
+    patente:"JJK116",
+    vendido:false
+ 
+    }
+     ]
+     module.exports=autos
+     
+     //cierro linea
